3412d5da8b6684544fb2b882f859a3ab9d0bf87c
Keith Hamm <hkhamm@gmail.com>
2020-11-11 16:51:44 -0800

    passing tests

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar"
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
 
 export default function App() {
+    const [text, setText] = useState("Welcome")
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text testID="welcome">{text}!!!</Text>
+            <Button title="Say Hello" testID="hello_button" onPress={() => setText("Hello")} />
+            <Button title="Say World" testID="world_button" onPress={() => setText("World")} />
             <StatusBar style="auto" />
         </View>
     )
